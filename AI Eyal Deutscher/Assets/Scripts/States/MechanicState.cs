9bf6eac430e81b6c80345683237409da95a4169b
EDeyal <75429849+EDeyal@users.noreply.github.com>
2023-03-19 14:45:34 +0200

    Update MechanicState.cs

--- AI Eyal Deutscher/Assets/Scripts/States/MechanicState.cs
+++ AI Eyal Deutscher/Assets/Scripts/States/MechanicState.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class MechanicState : MechanicBaseState
 {
-    protected CarrierStateHandler CarrierStateHandler => _stateHandler as CarrierStateHandler;
 
     public override BaseState RunCurrentState()
     {
